16fe6b51c85df985e34b477ec4a4dec89718c819
Vladimir Kolesnik <vkolesni@gmail.com>
2014-07-16 12:36:32 +0300

    Update fix_eth0.sh

--- fix_eth0.sh
+++ fix_eth0.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 # place to /sbin/ifup-local
 # chmod a+x /sbin/ifup-local
+# for RHEL
 
-#global script
+
 get_ip=$(hostname -I)
-
 if [[ ! $get_ip ]]; then
         current_mac=$(ifconfig -a | grep HWaddr | awk -F ' ' '{print $5}')
 
-        mac_in_config=$(cat /etc/sysconfig/networking/devices/ifcfg-eth0 | grep HWADDR | awk -F '=' '{print $2}')
+        if [[ $current_mac ]]; then
+                find_eth0_number=$(grep -rin 'eth0' /etc/udev/rules.d/70-persistent-net.rules | awk -F: '{print $1}')
 
-        if [[ $current_mac != $mac_in_config ]]; then
-		
-		
-                rm /etc/udev/rules.d/70-persistent-net.rules
-                #sed -i '2s/.*/HWADDR='${current_mac}'/' /etc/sysconfig/networking/devices/ifcfg-eth0
-                sed -i '2s/.*/HWADDR='${current_mac}'/' /etc/sysconfig/network-scripts/ifcfg-eth0
-				
-                reboot
+                if [[ $find_eth0_number ]]; then
+
+                        cat /etc/sysconfig/network-scripts/ifcfg-eth0 | grep -qi $current_mac
+
+                        if [ $? -eq 1 ]; then
+                                grep -v "eth0" /etc/udev/rules.d/70-persistent-net.rules > /etc/udev/rules.d/70-persistent-net.rules.temp
+                                mv -f :/etc/udev/rules.d/70-persistent-net.rules.temp /etc/udev/rules.d/70-persistent-net.rules
+                                sed -i 's/eth1/eth0/g' /etc/udev/rules.d/70-persistent-net.rules
+                                sed -i "7s/.*/HWADDR=$current_mac/" /etc/sysconfig/network-scripts/ifcfg-eth0
+                                reboot
+                        fi
+                fi
+
         fi
 fi
 
-# Only for eth0
+# Run this part of the script only with eth0 param
 if [[ "$1" == "eth0" ]]; then
-
-while true; do
-
         current_ip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 
         if [[ "$current_ip" =~ "172.28" ]]; then
                 sed -i '3s/.*/'${current_ip}' earchive.ucm11g earchive/' /etc/hosts
-                exit 1
-        else
-                sleep 5
-        fi
-
-done
+	fi
 
 fi
