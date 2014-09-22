5ef0d9ceffea4626b1dec92fa0b2dde8852162c3
Vladimir Kolesnik <vkolesni@gmail.com>
2014-09-22 09:36:36 +0300

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -9,44 +9,60 @@ for i in $(echo $MY_DATE | fold -w 1); do
 done
 
 #EXERCISE 1 START
-MY_NUMBER_1=$((((MY_DATE_SUM + RANDOM)) % 106))
-
-ipaddr="$(sed -n "${MY_NUMBER_1}p" names.txt)"
-
-if [[ "$ipaddr" ]]; then
-        echo "Got host $ipaddr"
-        find_ip_addr="$(dig +short $ipaddr)"
-        if [[ "$find_ip_addr" ]]; then
-                echo "Found IP: $find_ip_addr"
-                echo "Searching for additional names..."
-        else
-                echo "No IP was found for host: $ipaddr"
-        fi
-else
-        echo "Couldn't find IP"
-fi
-#EXERCISE 1 END
+echo "========================================================="
+echo "STARTING EXERCISE 1..."
+echo "========================================================="
+for ((i=1; i<=10; i++)); do
+         MY_NUMBER_1=$((((MY_DATE_SUM + RANDOM)) % 106))
+         
+         ipaddr="$(sed -n "${MY_NUMBER_1}p" names.txt)"
+         
+         if [[ "$ipaddr" ]]; then
+                 echo "Got host $ipaddr"
+                 find_ip_addr="$(dig +short $ipaddr)"
+                 if [[ "$find_ip_addr" ]]; then
+                         echo "Found IP: $find_ip_addr"
+                         echo "Searching for additional names..."
+                 else
+                         echo "No IP was found for host: $ipaddr"
+                 fi
+         else
+                 echo "Couldn't find IP"
+         fi
+done
+echo "========================================================="
+echo "EXERCISE 1 DONE"
+echo "========================================================="
 echo ''
+#EXERCISE 1 END
 #EXERCISE 2 START
-MY_NUMBER_2=$((((MY_DATE_SUM + RANDOM)) % 2000 + 400))
+echo "========================================================="
+echo "STARTING EXERCISE 2..."
+echo "========================================================="
 
-ipaddr="$(sed -n "${MY_NUMBER_2}p" ips.txt)"
-
-if [[ "$ipaddr" ]]; then
-        fixed_ipaddr=$(echo $ipaddr | grep -iPo '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-        if [[ "$fixed_ipaddr" ]]; then
-                echo "Got IP: $fixed_ipaddr"
-                find_ip_addr="$(dig +noall +short  +answer -x $fixed_ipaddr)"
-                if [[ "$find_ip_addr" ]]; then
-                        echo "Found hostname: $find_ip_addr"
-                        #echo "Searching for additional names..."
-                else
-                        echo "No hostname was found for IP: $fixed_ipaddr"
-                fi
-        else
-                echo 'Result from ips.txt is not IP address.'
-        fi
-else
-        echo "No result from ips.txt"
-fi
+for ((i=1; i<=20; i++)); do
+       MY_NUMBER_2=$((((MY_DATE_SUM + RANDOM)) % 2000 + 400))
+       ipaddr="$(sed -n "${MY_NUMBER_2}p" ips.txt)"
+       if [[ "$ipaddr" ]]; then
+               fixed_ipaddr=$(echo $ipaddr | grep -iPo '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+               if [[ "$fixed_ipaddr" ]]; then
+                       echo "Got IP: $fixed_ipaddr"
+                       find_ip_addr="$(dig +noall +short  +answer -x $fixed_ipaddr)"
+                       if [[ "$find_ip_addr" ]]; then
+                               echo "Found hostname: $find_ip_addr"
+                               #echo "Searching for additional names..."
+                       else
+                               echo "No hostname was found for IP: $fixed_ipaddr"
+                       fi
+               else
+                       echo 'Result from ips.txt is not IP address.'
+               fi
+       else
+               echo "No result from ips.txt"
+       fi
+done
+echo "========================================================="
+echo "EXERCISE 2 DONE"
+echo "========================================================="
+echo ''
 #EXERCISE 2 END
