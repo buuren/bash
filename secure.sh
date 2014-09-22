e8c8cd0ba7f4c28edc39548fe5dd7a11994742fe
Vladimir Kolesnik <vkolesni@gmail.com>
2014-09-22 10:45:55 +0300

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 MY_DATE='25'
-
+echo "My number is: $MY_DATE"
+echo ''
 MY_DATE_SUM=0
 for i in $(echo $MY_DATE | fold -w 1); do
         MY_DATE_SUM=$[$MY_DATE_SUM + $i]
 done
-
 #EXERCISE 1 START
 echo "========================================================="
 echo "STARTING EXERCISE 1..."
@@ -18,26 +18,32 @@ for ((i=1; i<=10; i++)); do
          
          if [[ "$ipaddr" ]]; then
                  echo "Got host $ipaddr"
-                 find_ip_addr="$(dig +short $ipaddr)"
+                 find_ip_addr="$(dig $ipaddr)"
                  if [[ "$find_ip_addr" ]]; then
                          echo "Found IP: $find_ip_addr"
-                         echo "Searching for additional names..."
+                         #echo "Searching for additional names..."
+                         
                  else
                          echo "No IP was found for host: $ipaddr"
                  fi
          else
                  echo "Couldn't find IP"
          fi
+         echo ""
 done
 echo "========================================================="
 echo "EXERCISE 1 DONE"
 echo "========================================================="
-echo ''
 #EXERCISE 1 END
+echo ''
 #EXERCISE 2 START
 echo "========================================================="
 echo "STARTING EXERCISE 2..."
 echo "========================================================="
+
+function establish_upstream () {
+         echo "$1"
+}
 
 for ((i=1; i<=20; i++)); do
        MY_NUMBER_2=$((((MY_DATE_SUM + RANDOM)) % 2000 + 400))
@@ -46,8 +52,11 @@ for ((i=1; i<=20; i++)); do
                fixed_ipaddr=$(echo $ipaddr | grep -iPo '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
                if [[ "$fixed_ipaddr" ]]; then
                        echo "Got IP: $fixed_ipaddr"
-                       find_ip_addr="$(dig +noall +short  +answer -x $fixed_ipaddr)"
+                       find_ip_addr="$(dig +noall +short +answer -x $fixed_ipaddr)"
                        if [[ "$find_ip_addr" ]]; then
+                               if [ "$i" -le '5' ]; then
+                                        establish_upstream 'hello'
+                               fi
                                echo "Found hostname: $find_ip_addr"
                                #echo "Searching for additional names..."
                        else
@@ -59,9 +68,31 @@ for ((i=1; i<=20; i++)); do
        else
                echo "No result from ips.txt"
        fi
+       echo ""
 done
 echo "========================================================="
 echo "EXERCISE 2 DONE"
 echo "========================================================="
-echo ''
 #EXERCISE 2 END
+echo ''
+#EXERCISE 4 START
+echo "========================================================="
+echo "STARTING EXERCISE 4..."
+echo "========================================================="
+MY_NUMBER_3=$((((MY_DATE_SUM + RANDOM)) % 3228 ))
+for ((i=1; i<=10; i++)); do
+       ip6addr="$(sed -n "${MY_NUMBER_3}p" ips6.txt)"
+       if [[ "$ip6addr" ]]; then
+               echo "$ip6addr"
+
+       else
+               echo "No result from ips.txt"
+       fi
+       ((MY_NUMBER_3=MY_NUMBER_3 + 40))
+       echo ""
+done
+echo "========================================================="
+echo "EXERCISE 4 DONE"
+echo "========================================================="
+echo ''
+#EXERCISE 4 END
